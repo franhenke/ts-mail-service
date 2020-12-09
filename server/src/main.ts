d7bc9e4d025072b5d8c3cb5aef4723bb92eb3fc1
franhenke <franci.henke@gmail.com>
2020-12-09 16:19:30 +0100

    Create REST Endpoints for app.get

--- server/src/main.ts
+++ server/src/main.ts
@@ -27,3 +27,44 @@ app.use(function (
   )
   inNext()
 })
+
+app.get('/mailboxes', async (inRequest: Request, inResponse: Response) => {
+  try {
+    const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo)
+    const mailboxes: IMAP.IMailbox[] = await imapWorker.listMailboxes()
+    inResponse.json(mailboxes)
+  } catch (inError) {
+    inResponse.send('error')
+  }
+})
+
+app.get(
+  '/mailboxes/:mailbox',
+  async (inRequest: Request, inResponse: Response) => {
+    try {
+      const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo)
+      const messages: IMAP.IMessage[] = await imapWorker.listMessages({
+        mailbox: inRequest.params.mailbox,
+      })
+      inResponse.json(messages)
+    } catch (inError) {
+      inResponse.send('error')
+    }
+  }
+)
+
+app.get(
+  '/mailboxes/:mailbox/:id',
+  async (inRequest: Request, inResponse: Response) => {
+    try {
+      const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo)
+      const messageBody: string = await imapWorker.getMessageBody({
+        mailbox: inRequest.params.mailbox,
+        id: parseInt(inRequest.params.id, 10),
+      })
+      inResponse.send(messageBody)
+    } catch (inError) {
+      inResponse.send('error')
+    }
+  }
+)
