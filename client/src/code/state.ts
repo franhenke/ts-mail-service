395e9176a2a8a6dc3a22bd4bde557e972365bab8
franhenke <franci.henke@gmail.com>
2020-12-12 21:41:29 +0100

    Add showComposeMessage switch and showAddContact

--- client/src/code/state.ts
+++ client/src/code/state.ts
@@ -1,3 +1,5 @@
+import { config } from './config'
+
 export function createState(inParentComponent) {
   return {
     // Flag: Is the please wait dialog visible?
@@ -24,6 +26,49 @@ export function createState(inParentComponent) {
 
     showHidePleaseWait: function (inVisible: boolean): void {
       this.setState({ pleaseWaitVisible: inVisible })
+    }.bind(inParentComponent),
+
+    showComposeMessage: function (inType: string): void {
+      switch (inType) {
+        case 'new':
+          this.setState({
+            currentView: 'compose',
+            messageTo: '',
+            messageSubject: '',
+            messageBody: '',
+            messageFrom: config.userEmail,
+          })
+          break
+
+        case 'reply':
+          this.setState({
+            currentView: 'compose',
+            messageTo: this.state.messageFrom,
+            messageSubject: `Re: ${this.state.messageSubject}`,
+            messageBody: `\n\n---- Original Message ----\n\n${this.state.messageBody}`,
+            messageFrom: config.userEmail,
+          })
+          break
+
+        case 'contact':
+          this.setState({
+            currentView: 'compose',
+            messageTo: this.state.contactEmail,
+            messageSubject: '',
+            messageBody: '',
+            messageFrom: config.userEmail,
+          })
+          break
+      }
+    }.bind(inParentComponent),
+
+    showAddContact: function (): void {
+      this.setState({
+        currentView: 'contactAdd',
+        contactID: null,
+        contactName: '',
+        contactEmail: '',
+      })
     }.bind(inParentComponent),
   }
 }
--- client/src/components/BaseLayout.tsx
+++ client/src/components/BaseLayout.tsx
@@ -1,58 +1,49 @@
-import {
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-} from '@material-ui/core'
-import { Dialog } from 'material-ui'
 import React, { Component } from 'react'
-import { createState } from '../state'
+
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+import { createState } from '../code/state'
+import Toolbar from './Toolbar'
+
 
 class BaseLayout extends Component {
   state = createState(this)
 
   render() {
+
     return (
-      <div className="appContainer">
-        <Dialog
-          open={this.state.pleaseWaitVisible}
-          disableBackdropClick={true}
-          disableEscapeKeyDown={true}
-          transitionDuration={0}
-        >
-          <DialogTitle style={{ textAlign: 'center' }}>Please Wait</DialogTitle>
-          <DialogContent>
-            <DialogContentText>...Connecting to server...</DialogContentText>
-          </DialogContent>
-        </Dialog>
 
-        <div className="toolbar">
-          <Toolbar state={this.state} />
-        </div>
+     <div className="appContainer">
 
-        <div className="mailboxList">
-          <MailboxList state={this.state} />
-        </div>
-        <div className="centerArea">
-          <div className="messageList">
-            <MessageList state={this.state} />
-          </div>
-        </div>
+      <Dialog open={ this.state.pleaseWaitVisible } disableBackdropClick={ true } disableEscapeKeyDown={ true }
+        transitionDuration={ 0 }>
+        <DialogTitle style={{ textAlign:"center" }}>Please Wait</DialogTitle>
+        <DialogContent><DialogContentText>...Contacting server...</DialogContentText></DialogContent>
+      </Dialog>
+
+       <div className="toolbar"><Toolbar state={ this.state } /></div>
+
+       <div className="mailboxList"><MailboxList state={ this.state } /></div>
+
+       <div className="centerArea">
+        <div className="messageList"><MessageList state={ this.state } /></div>
         <div className="centerViews">
-          {this.state.currentView === 'welcome' && <WelcomeView />}
-          {(this.state.currentView === 'message' ||
-            this.state.currentView === 'compose') && (
-            <MessageView state={this.state} />
-          )}
-          {(this.state.currentView === 'contact' ||
-            this.state.currentView === 'contactAdd') && (
-            <ContactView state={this.state} />
-          )}
+          { this.state.currentView === "welcome" && <WelcomeView /> }
+          { (this.state.currentView === "message" || this.state.currentView === "compose") &&
+            <MessageView state={ this.state } />
+          }
+          { (this.state.currentView === "contact" || this.state.currentView === "contactAdd") &&
+            <ContactView state={ this.state } />
+          }
         </div>
-        <div className="contactList">
-          <ContactList state={this.state} />
-        </div>
-      </div>
-    )
+       </div>
+
+       <div className="contactList"><ContactList state={ this.state } /></div>
+
+     </div>
   }
 }
 
