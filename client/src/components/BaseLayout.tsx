c82acaba680fa7d6d7a581c659333733129025c2
franhenke <franci.henke@gmail.com>
2020-12-12 21:11:33 +0100

    Adding conditional rendering to render currentView

--- client/src/components/BaseLayout.tsx
+++ client/src/components/BaseLayout.tsx
@@ -37,6 +37,20 @@ class BaseLayout extends Component {
             <MessageList state={this.state} />
           </div>
         </div>
+        <div className="centerViews">
+          {this.state.currentView === 'welcome' && <WelcomeView />}
+          {(this.state.currentView === 'message' ||
+            this.state.currentView === 'compose') && (
+            <MessageView state={this.state} />
+          )}
+          {(this.state.currentView === 'contact' ||
+            this.state.currentView === 'contactAdd') && (
+            <ContactView state={this.state} />
+          )}
+        </div>
+        <div className="contactList">
+          <ContactList state={this.state} />
+        </div>
       </div>
     )
   }
